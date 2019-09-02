eaa640ecc16bc1cac7478c894c66ed2cae734b42
597611084@qq.com <597611084@qq.com>
2019-09-02 21:07:56 +0800

    no message

--- src/main/java/com/barista/exception/GlobalExceptionHandler.java
+++ src/main/java/com/barista/exception/GlobalExceptionHandler.java
@@ -6,9 +6,8 @@ import com.barista.result.ResultCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * 全局异常处理
@@ -18,8 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @Date 2019/9/2 11:51
  * @Version 1.0
  */
-@ControllerAdvice
-@ResponseBody
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
